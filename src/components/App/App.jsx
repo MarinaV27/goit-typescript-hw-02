1653a9e86e4d997dcb7a03fa86810597d627fda8
MarinaV27 <Vasilishinamari@gmail.com>
2024-05-29 19:42:37 +0300

    add ErrorMessage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,24 @@ import { useEffect, useState } from "react"
 import SearchBar from "../SearchBar/SearchBar"
 import ImageGallery from "../ImageGallery/ImageGallery";
 import getImages from "../../images-api"
+import Loader from "../Loader/Loader";
+
 
 export default function App() {
     const [images, setImages] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
         async function fetchImages() {
-            const fetchImages = await getImages();
-            setImages(fetchImages);
+            try {
+                setIsLoading(true);
+                const fetchImages = await getImages();
+                setIsLoading(false);
+                setImages(fetchImages);
+            } caches {
+
+            }
         }
         fetchImages()
     }, []);
@@ -19,6 +29,8 @@ export default function App() {
         <div>
             <SearchBar />
             {images.length > 0 && <ImageGallery items={images} />}
+            {isError && <ErrorMessage />}
+            {isLoading && <Loader />}
         </div>
     )
 }
---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+export default function ErrorMessage(){
+    return  (
+    <p>Oops! There was an error! Try reloading</p>
+)
+}
