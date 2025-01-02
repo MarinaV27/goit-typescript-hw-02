60107a439747080d89ac9c96c04f02a77f8ff592
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-02 22:27:17 +0200

    loader css

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,19 @@
-import { Audio } from 'react-loader-spinner'
+import { Audio } from "react-loader-spinner";
+import css from "./Leader.module.css";
 
 export default function Loader() {
-    return (
-        <div>
-            <Audio
-                height="80"
-                width="80"
-                radius="9"
-                color="green"
-                ariaLabel="loading"
-                wrapperStyle
-                wrapperClass
-            />
-            <p>Loading data, please wait...</p>
-        </div>
-    )
+  return (
+    <div className={css.loadbox}>
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="loading"
+        wrapperStyle
+        wrapperClass
+      />
+      <p>Loading data, please wait...</p>
+    </div>
+  );
 }
