dec4602b52ead4eb6350e7c52af222cfd6b81f26
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-02 20:27:53 +0200

    add Toaster in App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,57 +1,59 @@
-
-import { useEffect, useState } from "react"
-import SearchBar from "../SearchBar/SearchBar"
+import { useEffect, useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
 import ImageGallery from "../ImageGallery/ImageGallery";
-import {getImages} from "../../images-api"
+import { getImages } from "../../images-api";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
-
+import { Toaster } from "react-hot-toast";
 
 export default function App() {
-    const [images, setImages] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
-    const [page, setPage] = useState(1);
-    const [searchQuery, setSearchQuery] = useState('');
-    const [totalPage] = useState(12);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [totalPage] = useState(12);
 
-    useEffect(() => {
-        if (searchQuery.trim() === "") {
-            return;
-        }
-        async function fetchImages() {
-            try {
-                setIsLoading(true);
-                setIsError(false);
-                const fetchImages = await getImages(searchQuery, page, totalPage);
-                setImages((prevState) => [...prevState, ...fetchImages]);
-            } catch (error) {
-                setIsError(true);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchImages()
-    }, [page, searchQuery, totalPage]);
-
-    const handlerSearch = async (topic) => {
-        setSearchQuery(topic);
-        setPage(1);
-        setImages([]);
+  useEffect(() => {
+    if (searchQuery.trim() === "") {
+      return;
     }
+    async function fetchImages() {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+        const fetchImages = await getImages(searchQuery, page, totalPage);
+        setImages((prevState) => [...prevState, ...fetchImages]);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchImages();
+  }, [page, searchQuery, totalPage]);
 
-    const handleLoadMore = async () => {
-        setPage(totalPage => totalPage + 1);
-    };
+  const handlerSearch = async (topic) => {
+    setSearchQuery(topic);
+    setPage(1);
+    setImages([]);
+  };
 
-    return (
-        <div>
-            <SearchBar onSubmit={handlerSearch} />
-            {images.length > 0 && <ImageGallery items={images} />}
-            {isError && <ErrorMessage />}
-            {images.length > 0 && !isLoading && (<LoadMoreBtn onClick={handleLoadMore} />)}
-            {isLoading && <Loader />}
-        </div>
-    )
+  const handleLoadMore = async () => {
+    setPage((totalPage) => totalPage + 1);
+  };
+
+  return (
+    <div>
+      <SearchBar onSubmit={handlerSearch} />
+      <Toaster />
+      {images.length > 0 && <ImageGallery items={images} />}
+      {isError && <ErrorMessage />}
+      {images.length > 0 && !isLoading && (
+        <LoadMoreBtn onClick={handleLoadMore} />
+      )}
+      {isLoading && <Loader />}
+    </div>
+  );
 }
