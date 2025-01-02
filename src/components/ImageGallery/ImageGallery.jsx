47e813794d73138bafa2e07a16d4cf17b2177a0d
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-02 22:51:15 +0200

    key id

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,9 +3,9 @@ import css from "../ImageGallery/ImageGallery.module.css";
 
 export default function ImageGallery({ items, onImageClick }) {
   return (
-    <ul className={css.menuList}>
+    <ul className={css.list}>
       {items.map((item) => (
-        <li className={css.list} key={items.id}>
+        <li className={css.item} key={item.id}>
           <ImageCard image={item} onClick={() => onImageClick(item)} />
         </li>
       ))}
