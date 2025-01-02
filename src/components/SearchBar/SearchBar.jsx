408b7d35c1c4ff4b8d29cde49291b30a8e39d5bd
MarinaV27 <Vasilishinamari@gmail.com>
2025-01-02 20:21:00 +0200

    init error in SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
 import css from "./SearchBar.module.css";
-
+import { toast } from "react-hot-toast";
 
 export default function SearchBar({ onSubmit }) {
-   const [query, setQuery] = useState ("");
+  const [query, setQuery] = useState("");
 
-   const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    
-      onSubmit(query);
-      setQuery('');
-    };
-    return (
-      <header className={css.header}>
-        <form className={css.form} onSubmit={handleSubmit}>
-          <button className={css.btn} type="submit">
-           
-          </button>
-          <input
-            className={css.input}
-            type="text"
-            value={query}
-            onChange={event => setQuery(event.target.value)}
-            autoFocus
-            autoComplete="off"
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-}
+    if (query.trim() === "") {
+      toast.error("This didn't work.");
+      return;
+    }
 
-  
+    onSubmit(query);
+    setQuery("");
+  };
+  return (
+    <header className={css.header}>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <button className={css.btn} type="submit"></button>
+        <input
+          className={css.input}
+          type="text"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          autoFocus
+          autoComplete="off"
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+}
