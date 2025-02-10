9a0e51f12b02749a82d78b6b4a3a40e172aa4b70
MarinaV27 <Vasilishinamari@gmail.com>
2025-02-10 21:15:09 +0200

    images-ipi

--- src/images-api.ts
+++ src/images-api.ts
@@ -1,9 +1,13 @@
 import axios from "axios"
+import { Image } from "./types";
 
 axios.defaults.baseURL = "https://api.unsplash.com";
 
-export const getImages = async (topic, currentPage, total_page = 12) => {
-    const response = await axios.get("/search/photos", {
+export const getImages = async (
+    topic: string,
+    currentPage: number,
+    total_page: number = 12): Promise<Image[]> => {
+    const response = await axios.get<{ results: Image[]}>("/search/photos", {
         params: {
             query: topic,
             page: currentPage,
